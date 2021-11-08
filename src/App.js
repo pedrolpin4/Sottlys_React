7d38d05f748feed60c59b3cee151651f0995101d
liggia <liggia.cruz@gmail.com>
2021-11-08 12:45:06 -0300

    feat: add folders (components, pages, styles, App)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from "./pages/Home"
+import GlobalStyles from "./styles/GlobalStyles"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+export default function App() {
+    return(
+        <Router>
+            <GlobalStyles />
+            <Routes>
+                <Route path = "/" exact element = {<Home />} />
+            </Routes>
+        </Router>
+    )
+}
