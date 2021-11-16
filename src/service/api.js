2a0fba8870383cbf217230b6fd05b96a02d9f29f
Pedro Lucas <pedrolucaspina22@gmail.com>
2021-11-16 12:22:02 -0300

    fix: fix mobile sidebar layout

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "postgres://adaejocmexjqsb:d65fdf40686cfb0a23f7deed80fb55f69b844970d1616b2ca24bde2d24482e54@ec2-44-197-136-81.compute-1.amazonaws.com:5432/d54035rhoiodhv",
 })
 
 export default API;
