39ad5c543351afbf00175f8ca915806b6bf914a2
Pedro Lucas <pedrolucaspina22@gmail.com>
2021-11-16 12:30:20 -0300

    fix: fix server url

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "postgres://adaejocmexjqsb:d65fdf40686cfb0a23f7deed80fb55f69b844970d1616b2ca24bde2d24482e54@ec2-44-197-136-81.compute-1.amazonaws.com:5432/d54035rhoiodhv",
+    baseURL: "https://sottlys.herokuapp.com",
 })
 
 export default API;
